62d06e62d26c039aee1bfbf91121956a9a46ff73
ykst615 <excit1ng@126.com>
2019-01-31 14:15:23 +0800

    add tslint-config airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import * as React from 'react';
 import * as styles from './App.styl';
 
-import logo from './logo.svg';
-
-const a = { 
-  b: 5,
-}
+import logoSvg from './logo.svg';
 
 class App extends React.Component {
   public render() {
-    const b= a.b;
     return (
       <div className={styles.app}>
         <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
+          <img src={logoSvg} className={styles.appLogo} alt="logo" />
           <h1 className={styles.appTitle}>Welcome to React</h1>
         </header>
         <p className={styles.appIntro}>
-          To get started, edit <code>src/App.tsx</code> and save to reload. {b}
+          To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
     );
